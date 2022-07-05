89a7e7b6e334622a1a773a2d6316e624cdd247f9
Gabriela Chiquetto <gabrielachiquetto@gmail.com>
2022-07-04 22:22:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const movieImg = document.querySelector('.movie_box-image');
+const movieTitle = document.querySelector('.heading-2');
+const score = document.querySelector('.score');
+const key = process.env.API_KEY;
